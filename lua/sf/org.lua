272147b5f4bdbcad3835e58c495471192ab49d80
Justin Benge <jbenge1@arizona.edu>
2024-09-11 18:01:42 +0200

    Locals only

--- lua/sf/org.lua
+++ lua/sf/org.lua
@@ -46,7 +46,7 @@ end
 H.orgs = {}
 
 H.authorize_an_org = function(name)
-	local cmd = B:new():cmd("org"):act("login web"):addParams({ ["--alias"] = name }):build()
+	local cmd = B:new():cmd("org"):act("login web"):addParams({ ["--alias"] = name }):localOnly():build()
 	local err_msg = "Command failed: " .. cmd
 	U.job_call(cmd, nil, err_msg)
 end
